faf158a2c53593ba3f4aabde5748a620c051baa4
Kirill Sedin <k.sedin@alterosmart.com>
2021-04-15 12:57:33 +0500

    feat(routes): add route

--- internal/proxy/routes.go
+++ internal/proxy/routes.go
@@ -1,1 +1,35 @@
 package proxy
+
+import (
+	"github.com/Melenium2/inhuman-reverse-proxy/internal/proxy/storage"
+	"github.com/gofiber/fiber/v2"
+)
+
+func newProxy(storage storage.ProxyStorage) func(ctx *fiber.Ctx) error {
+	return func(ctx *fiber.Ctx) error {
+		var req RequestProxy
+		if err := req.UnmarshalJSON(ctx.Body()); err != nil {
+			return writeError(ctx, 400, err)
+		}
+		if err := storage.Set(ctx.Context(), req.Code, req.Address...); err != nil {
+			return writeError(ctx, 400, err)
+		}
+
+		return write(ctx, map[string]interface{}{
+			"status":  "ok",
+			"added":   len(req.Address),
+			"to_code": req.Code,
+		})
+	}
+}
+
+func write(ctx *fiber.Ctx, data interface{}) error {
+	return ctx.JSON(data)
+}
+
+func writeError(ctx *fiber.Ctx, code int, err error) error {
+	ctx.Status(code)
+	return write(ctx, map[string]interface{}{
+		"err": err,
+	})
+}
